2145a3aa8a98006fd0746871192e854e92e27aaf
ggjulio <juligonz@student.42.fr>
2021-01-21 13:55:27 +0100

    .

--- ex02/Zombie.cpp
+++ ex02/Zombie.cpp
@@ -6,18 +6,19 @@
 /*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/21 12:01:31 by juligonz          #+#    #+#             */
-/*   Updated: 2021/01/21 12:05:46 by juligonz         ###   ########.fr       */
+/*   Updated: 2021/01/21 13:46:09 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie::Zombie()
-{
+Zombie::Zombie(std::string name, ZombieType type){
+	
 }
 Zombie::~Zombie(){
 	
 }
+
 void Zombie::advert()
 {
 
--- ex02/Zombie.hpp
+++ ex02/Zombie.hpp
@@ -6,14 +6,25 @@
 /*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/21 12:00:07 by juligonz          #+#    #+#             */
-/*   Updated: 2021/01/21 12:02:48 by juligonz         ###   ########.fr       */
+/*   Updated: 2021/01/21 13:42:55 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <string>
+
+typedef enum e_zombieType { Walker, Runner, Crawler, Armored } ZombieType;
+
 class Zombie{
+	std:string _name;
+	ZombieType _type;
 
 public:
-	Zombie();
+	Zombie(std::string name, ZombieType type);
 	~Zombie();
 	void advert();
 };
+
+#endif
