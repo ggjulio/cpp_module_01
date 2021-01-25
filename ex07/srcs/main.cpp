31e876216b0a9c7c2cbdf6b94efc757985d7d640
ggjulio <juligonz@student.42.fr>
2021-01-25 14:43:00 +0100

    ex07 done

--- ex07/srcs/main.cpp
+++ ex07/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/25 03:08:05 by juligonz          #+#    #+#             */
-/*   Updated: 2021/01/25 13:14:46 by juligonz         ###   ########.fr       */
+/*   Updated: 2021/01/25 14:31:00 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,19 @@
 #include <sstream>
 #include <string>
 
+#include <streambuf>
+
 void replace(std::ifstream & in, std::ofstream & out,
 	std::string to_find, std::string new_str)
 {
-	std::ostringstream st;
+	std::ostringstream sstream;
 	std::string s;
-	
-	(void) out;
-	st << in.rdbuf();
-	s = st.str();
-	for (size_t i = 0; i < s.size() ; i+=0)
-	{
-		std::cout << "find:"  << s.substr(i);
-		std::size_t found = s.substr(i).find(to_find);
-		if (found != std::string::npos)
-		{
-			s.replace(i, to_find.size(), new_str);
-			std::cout << "|||| " << s;
-			i+=found+1;
-		}
-		else 
-			i++;
-		std::cout << std::endl;
-	}
+	std::size_t found;
+
+	sstream << in.rdbuf();
+	s = sstream.str();
+	while ((found = s.find(to_find)) != std::string::npos)
+			s.replace(found, to_find.size(), new_str);
 	out << s;
 }
 
